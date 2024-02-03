2c884b68cde2fa36637c6564454837af11ca4d70
Niteesh Reddy Adavelli <niteesh@Niteeshs-MacBook-Air.local>
2024-02-02 21:48:40 -0500

    env

--- src/components/GoogleMapComponent.js
+++ src/components/GoogleMapComponent.js
@@ -14,7 +14,7 @@ const MapComponent = () => {
 
   return (
     <LoadScript
-      googleMapsApiKey="AIzaSyBwpRl4mubi4uFL0jvUyLT4jyJR6PRjZ_A"
+      googleMapsApiKey={process.env.REACT_APP_API_KEY}
     >
       <GoogleMap
         mapContainerStyle={mapStyles}
